706dee37ad16ba5300d1bb1ae50dc9c8d140e0ab
namaskaram <namaskaram@TeamCity>
2023-04-26 17:42:55 +0000

    TeamCity change in 'Anewtodolist' project: project parameters were changed

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -12,6 +12,9 @@ accordingly, and delete the patch script.
 changeProject(DslContext.projectId) {
     params {
         add {
+            text("github_username", "kanak.raju73@gmail.com", label = "github username", display = ParameterDisplay.HIDDEN, allowEmpty = false)
+        }
+        add {
             password("github_token", "credentialsJSON:9f222af8-aac9-40d6-8236-5a289ba34bb8", label = "github token", display = ParameterDisplay.HIDDEN)
         }
     }
